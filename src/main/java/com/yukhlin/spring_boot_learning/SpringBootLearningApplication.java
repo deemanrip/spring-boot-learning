cd20ef52086fe819d0729b86f68cb1205d5a74fd
Dmitry Yukhlin <yukhlindyu@altarix.ru>
2019-02-01 12:12:35 +0400

    Initial commit

---
+++ src/main/java/com/yukhlin/spring_boot_learning/SpringBootLearningApplication.java
@@ -0,0 +1,14 @@
+package com.yukhlin.spring_boot_learning;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootLearningApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringBootLearningApplication.class, args);
+    }
+
+}
+
